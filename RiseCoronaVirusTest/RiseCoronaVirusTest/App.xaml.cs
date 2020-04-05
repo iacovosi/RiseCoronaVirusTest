052a8a176a3567b90e9a4369b18acc2fe529f80a
iacovosi <iacovos.ioannou@gmail.com>
2020-04-05 19:30:22 +0300

    Add project files.

---
+++ RiseCoronaVirusTest/RiseCoronaVirusTest/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RiseCoronaVirusTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //InitializeComponent();
+
+          //  MainPage = new MainPage();
+		  			var tabs = new TabbedPage ();
+			tabs.Children.Add(new WebPage("coronatest.ucy.ac.cy") { Title = "CoronoVirusTest" });
+			tabs.Children.Add(new WebPage("covid-19.rise.org.cy") { Title = "CovTracker" });
+			MainPage = tabs;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RiseCoronaVirusTest/RiseCoronaVirusTest/WebPage.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace RiseCoronaVirusTest
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage(string url)
+        {
+            var browser = new WebView();
+            browser.Source = "http://"+ url;
+            Content = browser;
+        }
+    }
+}
+
